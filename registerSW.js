afabfcc07a11ae786679dc318bdc026ffed9db0c
Shxs7 <shxs7@example.com>
2026-07-25 21:32:01 +0800

    Fix base path

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/cuoti/sw.js', { scope: '/cuoti/' })})}
